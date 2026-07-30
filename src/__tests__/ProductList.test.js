f7cf8dfb296278a37582b4bd29e9baf0ed02bcd8
Janne <radius@hotmail.fi>
2026-07-30 12:42:07 +0300

    Fix ProductList.test.js by wrapping in LanguageProvider & ToastProvider to fix CI build

--- src/__tests__/ProductList.test.js
+++ src/__tests__/ProductList.test.js
@@ -1,6 +1,8 @@
 import React from 'react';
-import { render, waitFor, act } from '@testing-library/react-native';
+import { render } from '@testing-library/react-native';
 import ProductList from '../components/ProductList';
+import { LanguageProvider } from '../contexts/LanguageContext';
+import { ToastProvider } from '../contexts/ToastContext';
 
 jest.mock('../lib/api', () => ({
   getFavorites: jest.fn(() => new Promise(resolve => setTimeout(() => resolve([]), 0))),
@@ -10,7 +12,13 @@ jest.mock('../lib/api', () => ({
 
 test('renders product list items', () => {
   const products = [{ id: '1', name: 'Test', price: '10€', short: 'desc' }];
-  const { getByText } = render(<ProductList products={products} />);
+  const { getByText } = render(
+    <LanguageProvider>
+      <ToastProvider>
+        <ProductList products={products} />
+      </ToastProvider>
+    </LanguageProvider>
+  );
   expect(getByText('Test')).toBeTruthy();
   expect(getByText('10€')).toBeTruthy();
 });
